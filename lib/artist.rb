6fb3fe1a6489d627214d29d80a93707185e78f6e
Sachin Mahajan <sachinm78@gmail.com>
2019-03-25 16:57:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,5 +20,21 @@ class Artist
     @@all
   end
   
-  def find_or_create_by_name
+ def self.find_or_create_by_name(name)   
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+
+  def self.find(name)     
+    self.all.detect { |artist| artist.name == name }
+  end
+
+  def self.create(name)   
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
 end
