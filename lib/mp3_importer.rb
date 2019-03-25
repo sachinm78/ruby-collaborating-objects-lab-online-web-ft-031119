4cd806e11782b341ef9ebce131195bd926c46f29
Sachin Mahajan <sachinm78@gmail.com>
2019-03-25 17:07:29 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,5 +5,19 @@ class MP3Importer
     @path = path
   end
   
+  def files
+    files = []
+    Dir.new(self.path).each do |file|
+      files << file if file.length > 4
+    end
+    files
+  end
+
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
+  end
+end
   
 end
