243593ed332a8799c77d499d0f0e176b170aeb80
Sachin Mahajan <sachinm78@gmail.com>
2019-03-25 16:51:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,5 @@
 class Song 
   attr_accessor :name, :artist 
 
-  @@all = []
-
-  def initialize(name, artist)
-    @name = name
-    @artist = artist
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
+  
 end
